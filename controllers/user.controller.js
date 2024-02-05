bb6534fc48cde57a09c6472e7a2473cf4e973202
nishant25062002 <mainwalnishant9@gmail.com>
2024-02-06 00:38:51 +0530

    added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -143,104 +143,131 @@ const verifyOtp = asyncHandler(async (req, res) => {
 });
 
 const updateUser = asyncHandler(async (req, res) => {
-  let user = req.user.toObject();
-  const { name, email, dob, aadhar, pan, category, fathersName } = req.body;
-  if (!name || !email || !pan || !aadhar || !dob || !category || !fathersName) {
+  try {
+    let user = req.user.toObject();
+    const { name, email, dob, aadhar, pan, category, fathersName } = req.body;
+    if (
+      !name ||
+      !email ||
+      !pan ||
+      !aadhar ||
+      !dob ||
+      !category ||
+      !fathersName
+    ) {
+      return res
+        .status(400)
+        .json({ error: "Please add all fields", status: false });
+    }
+
+    if (user?.isMobileNumberVerified) {
+      await User.findByIdAndUpdate(
+        { _id: user._id },
+        {
+          name,
+          email,
+          pan,
+          aadhar,
+          dob,
+          category,
+          fathersName,
+        }
+      );
+
+      //send email for User Updated
+      const mailOptions = {
+        from: "taxjugnoo@gmail.com",
+        to: email,
+        subject: "User Details Updated Successfully",
+        text: `Hi ${name},
+
+Your Details has been Verified Successfully .
+  
+Keep it safe! If you need help, reach out to us.
+  
+Best,
+Team Tax Jugnoo`,
+      };
+
+      sendEmail(mailOptions);
+      const updatedUser = await User.findOne({
+        mobileNumber: user.mobileNumber,
+      });
+
+      user = updatedUser.toObject();
+      delete user.otp;
+
+      return res.status(201).json({
+        data: user,
+        token: generateToken(user._id),
+        status: true,
+      });
+    } else {
+      return res.status(400).json({ error: "invalid user data" });
+    }
+  } catch (error) {
+    console.error("Error in updateUser:", error);
     return res
-      .status(400)
-      .json({ error: "Please add all fields", status: false });
-  }
-
-  if (user?.isMobileNumberVerified) {
-    await User.findByIdAndUpdate(
-      { _id: user._id },
-      {
-        name,
-        email,
-        pan,
-        aadhar,
-        dob,
-        category,
-        fathersName,
-      }
-    );
-
-    //send email for User Updated
-    const mailOptions = {
-      from: "taxjugnoo@gmail.com",
-      to: email,
-      subject: "User Details Updated Successfully",
-      text: `Hi ${name},
-
-    Your Details has been Verified Successfully .
-    
-    Keep it safe! If you need help, reach out to us.
-    
-    Best,
-    Team Tax Jugnoo`,
-    };
-
-    sendEmail(mailOptions);
-    const updatedUser = await User.findOne({ mobileNumber: user.mobileNumber });
-
-    user = updatedUser.toObject();
-    delete user.otp;
-
-    return res.status(201).json({
-      data: user,
-      token: generateToken(user._id),
-      status: true,
-    });
-  } else {
-    return res.status(400).json({ error: "invalid user data" });
+      .status(500)
+      .json({ error: "Internal Server Error", status: false });
   }
 });
 
 const addIdUser = asyncHandler(async (req, res) => {
-  let user = req.user.toObject();
-  const { name, information } = req.body;
-  if (!name || !information) {
+  try {
+    let user = req.user.toObject();
+    const { name, information } = req.body;
+
+    if (!name || !information) {
+      return res
+        .status(400)
+        .json({ error: "Please add all fields", status: false });
+    }
+
+    if (user?.isMobileNumberVerified) {
+      await User.findByIdAndUpdate(
+        { _id: user._id },
+        {
+          ids: [...user.ids, { name, information }],
+        }
+      );
+
+      //send email for User Updated
+      const mailOptions = {
+        from: "taxjugnoo@gmail.com",
+        to: user.email,
+        subject: "User Details Updated Successfully",
+        text: `Hi ${user.name},
+
+  Your Details has been Verified Successfully .
+  
+  Keep it safe! If you need help, reach out to us.
+  
+  Best,
+  Team Tax Jugnoo`,
+      };
+
+      sendEmail(mailOptions);
+      const updatedUser = await User.findOne({
+        mobileNumber: user.mobileNumber,
+      });
+
+      user = updatedUser.toObject();
+      delete user.otp;
+
+      return res.status(201).json({
+        data: user,
+        token: generateToken(user._id),
+        status: true,
+      });
+    } else {
+      return res.status(400).json({ error: "invalid user data" });
+    }
+  } catch (error) {
+    console.error("Error in addIdUser:", error);
     return res
-      .status(400)
-      .json({ error: "Please add all fields", status: false });
-  }
-
-  if (user?.isMobileNumberVerified) {
-    await User.findByIdAndUpdate(
-      { _id: user._id },
-      {
-        ids: [...user.ids, { name, information }],
-      }
-    );
-
-    //send email for User Updated
-    const mailOptions = {
-      from: "taxjugnoo@gmail.com",
-      to: user.email,
-      subject: "User Details Updated Successfully",
-      text: `Hi ${user.name},
-
-    Your Details has been Verified Successfully .
-    
-    Keep it safe! If you need help, reach out to us.
-    
-    Best,
-    Team Tax Jugnoo`,
-    };
-
-    sendEmail(mailOptions);
-    const updatedUser = await User.findOne({ mobileNumber: user.mobileNumber });
-
-    user = updatedUser.toObject();
-    delete user.otp;
-
-    return res.status(201).json({
-      data: user,
-      token: generateToken(user._id),
-      status: true,
-    });
-  } else {
-    return res.status(400).json({ error: "invalid user data" });
+      .status(500)
+      .json({ error: "Internal Server Error", status: false });
   }
 });
 
