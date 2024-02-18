27042474bb5bb8c63b838875f74b2022a21c2ecd
nishant25062002 <mainwalnishant9@gmail.com>
2024-02-18 18:39:47 +0530

    Added address

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -159,7 +159,7 @@ const verifyOtp = asyncHandler(async (req, res) => {
 const updateUser = asyncHandler(async (req, res) => {
   try {
     let user = req.user.toObject();
-    const { name, email, dob, pan, category, fathersName } = req.body;
+    const { name, email, dob, pan, category, address, fathersName } = req.body;
     if (!name || !email) {
       return res
         .status(400)
@@ -175,6 +175,7 @@ const updateUser = asyncHandler(async (req, res) => {
           pan,
           dob,
           category,
+          address,
           fathersName,
         }
       );
